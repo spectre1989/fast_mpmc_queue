7520a93a7a95cc6a8bbb276d4a5bc3dc11350798
Joe Best-Rotheray <spectre1989@gmail.com>
2016-02-14 22:35:36 +0000

    now using google charts to show the test results, tidied up the test functions, now need to work on completing the actual queue, and figure out why its performance is awful

--- Lock Free MPMC Queue/Lock Free MPMC Queue/Tests/Main.cpp
+++ Lock Free MPMC Queue/Lock Free MPMC Queue/Tests/Main.cpp
@@ -18,7 +18,7 @@ struct PaddedValue
     operator size_t() const { return value; }
 };
 
-template <typename Value, typename Queue> std::chrono::microseconds::rep test(const size_t num_threads, char* memory, const size_t num_values, Queue& queue)
+template <template<typename, typename> class Queue, typename Value, typename IndexType> std::chrono::microseconds::rep test(const size_t num_threads, char* memory, const size_t num_values, Queue<Value, IndexType>& queue)
 {
 	memset(memory, 0, sizeof(char) * num_values);
 
@@ -85,68 +85,88 @@ template <typename Value, typename Queue> std::chrono::microseconds::rep test(co
 		printf("FAIL!\n");
 	}
 
-	return std::chrono::duration_cast<std::chrono::microseconds>(time_taken).count();
+	return std::chrono::duration_cast<std::chrono::milliseconds>(time_taken).count();
 }
 
-template <typename Queue> std::vector<std::pair<size_t, std::vector<double>>> test_batch(const size_t num_threads_max, const size_t num_values, const size_t queue_size, const size_t num_samples, char* memory)
+template <class Queue>
+std::vector<double> test_batch(const size_t num_threads_max, const size_t num_values, const size_t queue_size, const size_t num_samples, char* memory)
 {
-	std::vector<std::pair<size_t, std::vector<double>>> results;
-
-	Queue queue(queue_size);
-
-	for(size_t num_threads = 1; num_threads <= num_threads_max; num_threads *= 2)
-	{
-		std::vector<double> samples(num_samples);
-
-		for(size_t i = 0; i < num_samples; ++i)
-		{
-			samples.push_back(test(num_threads, memory, num_values, queue));
-		}
-
-		results.push_back(std::make_pair(num_threads, std::move(samples)));
-	}
-
-	return results;
+    std::vector<double> results;
+    Queue queue(queue_size);
+    double inv_num_samples = 1.0 / double(num_samples);
+    double avg_time_taken = 0.0;
+    
+    for (size_t num_threads = 1; num_threads <= num_threads_max; num_threads *= 2)
+    {
+        for (size_t i = 0; i < num_samples; ++i)
+        {
+            avg_time_taken += test(num_threads, memory, num_values, queue) * inv_num_samples;
+        }
+        
+        results.push_back(avg_time_taken);
+    }
+    
+    return results;
 }
 
 // TODO
-// timing data
 // mutex queue which allows simultaneous read/write
 // deal with index wrap around
 
 int main(int argc, char* argv[])
 {
 	const size_t num_threads_max = 16;
-	const size_t num_values = 1 << 12;
+    const size_t num_values = 1 << 9;//1 << 12;
 	const size_t queue_size = 32;
-	const size_t num_samples = 128;
+    const size_t num_samples = 10;//128;
 
+    std::vector<double> results[2];
+    
 	char* memory = new char[num_values];
 
-	{
-		MutexQueue<PaddedValue, std::uint32_t> queue(queue_size);
-		double inv_num_samples = 1.0 / (double)num_samples;
-		double avg_time_taken = 0.0;
-		for (size_t num_threads = 1; num_threads <= num_threads_max; num_threads *= 2)
-		{
-			for (size_t i = 0; i < num_samples; ++i)
-			{
-				avg_time_taken += test<PaddedValue>(num_threads, memory, num_values, queue) * inv_num_samples;
-			}
-		}
-	}
-	{
-		LockFreeMPMCQueue<PaddedValue, std::uint32_t> queue(queue_size);
-		for (size_t num_threads = 1; num_threads <= num_threads_max; num_threads *= 2)
-		{
-			for (size_t i = 0; i < num_samples; ++i)
-			{
-				test<PaddedValue>(num_threads, memory, num_values, queue);
-			}
-		}
-	}
-
-	printf("Done!\n");
+    results[0] = test_batch<LockFreeMPMCQueue<size_t, std::uint32_t>>(num_threads_max, num_values, queue_size, num_samples, memory);
+    results[1] = test_batch<MutexQueue<size_t, std::uint32_t>>(num_threads_max, num_values, queue_size, num_samples, memory);
+    
+    printf("<html>\n");
+    printf("<head>\n");
+    printf("<!--Load the AJAX API-->\n");
+    printf("<script type=\"text/javascript\" src=\"https://www.gstatic.com/charts/loader.js\"></script>\n");
+    printf("<script type=\"text/javascript\">\n\n");
+    printf("google.charts.load('current', {'packages':['corechart', 'line']});\n\n");
+    printf("google.charts.setOnLoadCallback(drawChart);\n\n");
+    printf("function drawChart() {\n");
+    printf("var data = new google.visualization.DataTable();\n");
+    printf("data.addColumn('number', 'X');\n");
+    printf("data.addColumn('number', 'LockFreeMPMCQueue');\n");
+    printf("data.addColumn('number', 'MutexQueue');\n");
+    printf("data.addRows([\n");
+    
+    for(size_t i = 0, num_threads = 1; i < results[0].size(); ++i, num_threads *= 2)
+    {
+        printf("[%zu,%f,%f]", num_threads, results[0][i], results[1][i]);
+        if(i < (results[0].size() - 1))
+        {
+            printf(",");
+        }
+        printf("\n");
+    }
+    
+    printf("]);\n");
+    printf("var options = {\n");
+    printf("hAxis: {title: 'Threads'},\n");
+    printf("vAxis: {title: 'Avg Time Taken (milliseconds)'},\n");
+    printf("series: {1: {curveType: 'function'}},\n");
+    printf("height: 800\n");
+    printf("};\n");
+    printf("var chart = new google.visualization.LineChart(document.getElementById('chart_div'));\n");
+    printf("chart.draw(data, options);\n");
+    printf("}\n");
+    printf("</script>\n");
+    printf("</head>\n");
+    printf("<body>\n");
+    printf("<div id=\"chart_div\"></div>\n");
+    printf("</body>\n");
+    printf("</html>\n");
 
 	delete[] memory;
 
